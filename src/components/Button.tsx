ae5f2abd97fe262bb9bdd7c48d08fdc401625d3d
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-11-03 18:03:01 +0100

    Fix styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
-const Button = ({ styles }: {[key:string]: string}) => {
-  return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>Get Started</button>
-  )
-}
+interface ButtonProps {
+  className?: string;
+  children?: React.ReactNode;
+};
+
+const Button = ({ className = '', children='Get Started' }: ButtonProps) => (
+  <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none ${className} rounded-lg`}>{children}</button>
+  );
 
 export default Button;
--- src/components/CardDeal.tsx
+++ src/components/CardDeal.tsx
@@ -3,16 +3,16 @@ import styles, {layout} from "../style";
 import Button from "./Button";
 
 const CardDeal = () => (
-    <section className={layout.section}>
-      <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Find better card deal<br className="sm:block hidden"/>in a few easy steps.</h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Acru tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.c Aliquet ultices ac, ametau.</p>
-        <Button styles='mt-10' />
-      </div>
-      <div className={layout.sectionImg}>
-        <img src={card} alt="card" className="w-[100%] h-[100%]"/>
-      </div>
-    </section>
-  )
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find better card deal<br className="sm:block hidden"/>in a few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-lg mt-5`}>Quickly discover card options tailored to your needs. Compare rates, benefits, and rewards with ease to find the perfect card that fits your lifestyle—all in just a few clicks.</p>
+      <Button className='mt-10' />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="Credit card illustration" className="w-full h-full"/>
+    </div>
+  </section>
+  );
 
 export default CardDeal;
